a202b629aa36b5c0553d8c489f68a8587186e0b3
naveen962 <mrn28897@gmail.com>
2021-09-15 07:46:02 -0700

    creating a program to calculate total working hours or days reached for a month

--- twentydailywages.sh
+++ twentydailywages.sh
@@ -1,10 +1,45 @@
-#! /bin/bash
 
-wagesperday=20
+#! /bin/bash
+wageperhour=20
 dayspermonth=20
-for (( i=1; i<=$dayspermonth; i++ ))
+hourspermonth=100
+fulltime=8
+parttime=4
+TotalWorkHour=0
+i=0
+while :
 do
-   hours=$((RANDOM%5))
-dailywages=$(($hours * $wagesperday))
-echo "day $i:" $dailywages
+attendance=$(( RANDOM%3 ))
+if [[ $attendance == 0 ]]
+then
+	echo "fulltime"
+i=$(($i + 1))
+	echo "day $i "
+	TotalWorkHour=$(( $TotalWorkHour + $fulltime ))
+	echo "TotalWorkHour :" $TotalWorkHour
+   dailywages=$(( $wageperhour * $fulltime ))
+			echo $dailywages
+	if [ $i -eq $dayspermonth ] || [ $TotalWorkHour -ge $hourspermonth ]
+	then
+		break
+	fi
+
+elif [[ $attendance == 1 ]]
+then
+echo "parttime"
+i=$(($i + 1))
+ echo "day $i "
+   TotalWorkHour=$(( $TotalWorkHour + $parttime ))
+	  echo "TotalWorkHour :" $TotalWorkHour
+   dailywages=$(( $wageperhour * $parttime ))
+         echo $dailywages
+   if [ $i -eq $dayspermonth ] || [ $TotalWorkHour -ge $hourspermonth ]
+   then
+      break
+   fi
+else
+   echo "absent"
+fi
 done
+
+
