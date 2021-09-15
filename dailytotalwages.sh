2da934ba2012d8eca7fb65770874d296d0764c6b
naveen962 <mrn28897@gmail.com>
2021-09-15 11:13:43 -0700

    day and dailywage along with totalwage added

--- dailytotalwages.sh
+++ dailytotalwages.sh
@@ -1,12 +1,31 @@
 #! /bin/bash
-wagesperday=20
 
+wageperhour=20
+fulltime=8
+parttime=4
 dayspermonth=20
 for (( i=1; i<=20; i++ ))
 do
-hours=$((RANDOM%8))
-dailywage=$(($hours * $wagesperday))
-echo "day $i : "$dailywage
-TotalWage=$(($dayspermonth * $dailywage))
-echo Total Wage:"$TotalWage"
+attendance=$((RANDOM%3))
+
+if [[ $attendance == 0 ]]
+then 
+	echo "fulltime"
+	dailywages=$(( $wageperhour * $fulltime ))
+	echo "day $i:"$dailywages
+Totalwage=$(( $dayspermonth * $dailywages ))
+
+ 	Totalwage=$(( $Totalwage + $dailywages ))
+echo "Totalwages :" $Totalwage
+elif [[ $attendance == 1 ]]
+then
+	echo "parttime"
+	dailywages=$(( $wageperhour * $parttime ))
+	echo "day $i:" $dailywages
+Totalwage=$(( $dayspermonth * $dailywages ))
+	 Totalwage=$(( $Totalwage + $dailywages ))
+	echo "Totalwages :" $Totalwage
+else
+	echo	"day $i: absent"
+fi 
 done
