09dd9d5e3217b9f7aa508775b39e1f26f89ee426
naveen962 <mrn28897@gmail.com>
2021-09-15 11:40:38 -0700

    monthly wages program added

--- monthlywages.sh
+++ monthlywages.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
 
-wageperday=20
-totalhours=8
-dailywage=$(($wageperday * $totalhours))
+
+
 dayspermonth=20
-monthlywages=$(($dayspermonth * $dailywage))
-echo $monthlywages
+wageperhour=20
+fulltime=8
+parttime=4
+attendance=$((RANDOM%3))
+if [[ $attendance == 0 ]]
+then 
+echo "fulltime"
+	dailywages=$(( $wageperhour * $fulltime ))
+echo "dailywage :" $dailywages
+monthlywages=$(($dayspermonth * $dailywages))
+echo "monthlywages :"$monthlywages
+
+elif [[ $attendance == 1 ]]
+then
+echo "parttime"
+	dailywages=$(( $wageperhour * $parttime ))
+echo "dailywage :"$dailywages
+monthlywages=$(($dayspermonth * $dailywages))
+echo  "monthlywages :"$monthlywages
+
+else
+echo	"absent"
+fi 
